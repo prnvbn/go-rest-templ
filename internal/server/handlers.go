d48c80d538c7db49c48a7abbcf94146a2e6ff36c
prnvbn <prnvbn@gmail.com>
2025-05-05 00:22:00 +0100

    Add graceful shutdown

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -7,27 +7,27 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func (s Server) homePageHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Server) homePageHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, world!"))
 }
 
 // nameHandler is a handler for the /{name} endpoint.
 // It takes a name as a path parameter and returns a greeting
-func (s Server) nameHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Server) nameHandler(w http.ResponseWriter, r *http.Request) {
 	name := chi.URLParam(r, "name")
 	w.Write([]byte("Hello, " + name + "!"))
 }
 
 // nameAsParamHandler is a handler for the /query?name={name} endpoint.
 // It takes a name as a query parameter and returns a greeting
-func (s Server) nameAsParamHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Server) nameAsParamHandler(w http.ResponseWriter, r *http.Request) {
 	// Get the value of the "name" query parameter from the URL.
 	name := r.URL.Query().Get("name")
 
 	w.Write([]byte("Hello, " + name + ", You were passed in as a query!"))
 }
 
-func (s Server) catFactHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Server) catFactHandler(w http.ResponseWriter, r *http.Request) {
 	cr, err := cat.FetchFact(s.cfg.CatFact.URL)
 	if err != nil {
 		respondWithError(w, err)
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,8 +1,13 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -46,6 +51,31 @@ func (s *Server) initRoutes() {
 
 func (s *Server) Run() {
 	addr := fmt.Sprintf("%s:%d", s.cfg.Addr, s.cfg.Port)
-	log.Info().Str("addr", addr).Msg("Serving API")
-	log.Fatal().Err(http.ListenAndServe(addr, s)).Send()
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: s,
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		log.Info().Str("addr", addr).Msg("Serving API")
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Error().Err(err).Msg("Server error")
+		}
+	}()
+
+	<-ctx.Done()
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Attempt graceful shutdown
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		log.Error().Err(err).Msg("Graceful shutdown did not complete")
+		if err := srv.Close(); err != nil {
+			log.Error().Err(err).Msg("Could not stop server")
+		}
+	}
 }
